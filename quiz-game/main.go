a04094d46529e9139c7745b5530c6b7ae7defa84
Richard Githuba <githuba9520@gmail.com>
2024-08-01 10:06:27 +0300

    opening the csv file

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -9,7 +9,24 @@ import (
 	"strings"
 )
 
+// function to exit in case of an error
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv in the formart of 'question, answer'")
 	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the csv file : %s\n", *csvFilename))
+	}
+	defer file.Close()
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided csv file")
+	}
 }
